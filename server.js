98abf6ea874031561cfd1eafa2126d656307da4d
Keith Hamasaki <khamasaki@teampraxis.com>
2015-11-11 15:13:16 -1000

    Added rudimentary support for sendRawEmail.

--- server.js
+++ server.js
@@ -22,34 +22,47 @@ var responseTemplate = fs.readFileSync(__dirname + '/response_template.txt', { e
 var errorTemplate = fs.readFileSync(__dirname + '/error_template.txt', { encoding: 'utf-8' });
 
 app.use(function (req, res) {
-  // for now we only support sending
-  if (req.body.Action !== 'SendEmail') {
-    res.status(400).send('Invalid action: ' + req.body.Action);
-  }
-
-  // log recipient, subject, and body length to console
-  console.log('Email received:');
-  console.log({
-    Source: req.body.Source,
-    Destination: req.body['Destination.ToAddresses.member.1'],
-    Subject: req.body['Message.Subject.Data'],
-    HtmlSize: req.body['Message.Body.Html.Data'].length,
-    TextSize: req.body['Message.Body.Text.Data'].length
-  });
-
-  // save the email to the output folder
   var id = uuid.v4();
-  try {
-    mkdirp.sync(output + '/' + id);
-    fs.writeFileSync(output + '/' + id + '/content.html', req.body['Message.Body.Html.Data']);
-    fs.writeFileSync(output + '/' + id + '/content.txt', req.body['Message.Body.Text.Data']);
-    fs.writeFileSync(output + '/' + id + '/subject.txt', req.body['Message.Subject.Data']);
-    fs.writeFileSync(output + '/' + id + '/destination.txt', req.body['Destination.ToAddresses.member.1']);
-    fs.writeFileSync(output + '/' + id + '/source.txt', req.body.Source);
-    res.status(200).send(responseTemplate.replace('@@@messageId@@@', id));
-  } catch (err) {
-    console.log('Error saving email', err);
-    res.status(500).send(errorTemplate.replace('@@@code@@@', 'MessageRejected').replace('@@@message@@@', err.message));
+
+  // for now we only support sending
+  if (req.body.Action === 'SendEmail') {
+    // log recipient, subject, and body length to console
+    console.log('Email received:');
+    console.log({
+      Source: req.body.Source,
+      Destination: req.body['Destination.ToAddresses.member.1'],
+      Subject: req.body['Message.Subject.Data'],
+      HtmlSize: req.body['Message.Body.Html.Data'].length,
+      TextSize: req.body['Message.Body.Text.Data'].length
+    });
+
+    // save the email to the output folder
+    try {
+      mkdirp.sync(output + '/' + id);
+      fs.writeFileSync(output + '/' + id + '/content.html', req.body['Message.Body.Html.Data']);
+      fs.writeFileSync(output + '/' + id + '/content.txt', req.body['Message.Body.Text.Data']);
+      fs.writeFileSync(output + '/' + id + '/subject.txt', req.body['Message.Subject.Data']);
+      fs.writeFileSync(output + '/' + id + '/destination.txt', req.body['Destination.ToAddresses.member.1']);
+      fs.writeFileSync(output + '/' + id + '/source.txt', req.body.Source);
+      res.status(200).send(responseTemplate.replace('@@@messageId@@@', id));
+    } catch (err) {
+      console.log('Error saving email', err);
+      res.status(500).send(errorTemplate.replace('@@@code@@@', 'MessageRejected').replace('@@@message@@@', err.message));
+    }
+  } else if (req.body.Action === 'SendRawEmail') {
+    console.log('Raw Email received');
+
+    // save the email to the output folder
+    try {
+      mkdirp.sync(output + '/' + id);
+      fs.writeFileSync(output + '/' + id + '/content.txt', req.body['RawMessage.Data']);
+      res.status(200).send(responseTemplate.replace('@@@messageId@@@', id));
+    } catch (err) {
+      console.log('Error saving email', err);
+      res.status(500).send(errorTemplate.replace('@@@code@@@', 'MessageRejected').replace('@@@message@@@', err.message));
+    }
+  } else {
+    res.status(400).send('Invalid action: ' + req.body.Action);
   }
 });
 
